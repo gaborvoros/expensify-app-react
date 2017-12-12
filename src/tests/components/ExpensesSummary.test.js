844ba7b90d7f0dee98f72638961a8e4c413bb39b
Gabor Voros <gabor.voros@valtech.de>
2017-12-12 16:53:51 +0100

    setting up expensesummary with tests

---
+++ src/tests/components/ExpensesSummary.test.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import {ExpenseSummary} from '../../components/ExpensesSummary'
+
+test('should correctly render ExpenseSummary with 1 expense', () => {
+
+})
