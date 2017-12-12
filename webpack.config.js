26a8a9c835d882e33830f37b3687c0d55713f783
Gabor Voros <gabor.voros@valtech.de>
2017-12-12 13:24:30 +0100

    setting up production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,42 @@
 
 var path = require('path');
 
-module.exports = {
-	entry: './src/app.js',
-	output: {
-		path: path.join(__dirname, 'public'),
-		filename: 'bundle.js'
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.jsx?$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-				query: {
-					cacheDirectory: true,
-					presets: ['react', 'es2015', 'env'],
-					plugins: ['transform-class-properties']
-				}
-			},
-			{
-				test: /\.s?css$/,
-				use: [
+module.exports = (env) => {
+	const isProduction = env === 'production';
+
+	return {
+		entry: './src/app.js',
+		output: {
+			path: path.join(__dirname, 'public'),
+			filename: 'bundle.js'
+		},
+		module: {
+			loaders: [
+				{
+					test: /\.jsx?$/,
+					loader: 'babel-loader',
+					exclude: /node_modules/,
+					query: {
+						cacheDirectory: true,
+						presets: ['react', 'es2015', 'env'],
+						plugins: ['transform-class-properties']
+					}
+				},
+				{
+					test: /\.s?css$/,
+					use: [
 						'style-loader',
 						'css-loader',
 						'sass-loader'
-				]
-			}
-		]
-	},
-	devtool: 'cheap-module-eval-source-map',
-	devServer: {
-		contentBase: path.join(__dirname, 'public'),
-		historyApiFallback: true
-	}
+					]
+				}
+			]
+		},
+		devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+		devServer: {
+			contentBase: path.join(__dirname, 'public'),
+			historyApiFallback: true
+		}
+	};
 };
+
