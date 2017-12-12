01ec84280295c5ee3982041c863a9558f6c6c24f
Gabor Voros <gabor.voros@valtech.de>
2017-12-12 14:22:53 +0100

    create prod server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+//get plugins
+const path = require('path');
+const express = require('express');
+const app = express();
+//path to public folder where the build files are
+const publicPath = path.join(__dirname, '..', 'public');
+
+app.use(express.static(publicPath));
+
+//as there are no html subpages and subfolders on prod like 'localhost:3000/create', it will get an error upon pagerefresh
+app.get('*', (req, res) => {
+	//process all unhandled requests from index.html
+	res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+//create server on port 3000
+app.listen(3000, () => {
+	console.log('server runs');
+});
