b8f64f135a180a528afd08a1ce08f8762f7391c5
Gabor Voros <gabor.voros@valtech.de>
2017-12-15 11:55:01 +0100

    setup logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import {Link, NavLink} from 'react-router-dom';
+import {connect} from 'react-redux'
+import {startLogout} from '../actions/auth'
 
-const Header = () => (
+export const Header = ({startLogout}) => (
 		<header>
 			<h1>Expensify</h1>
 			<NavLink to="/" exact={true}>Home</NavLink>
 			<NavLink to="/create">Create Expense</NavLink>
 			<NavLink to="/help">Help</NavLink>
+			<button onClick={startLogout}>Logout</button>
 		</header>
 );
 
-export default Header;
+const mapDispatchToProps = (dispatch) => ({
+	startLogout: () => dispatch(startLogout())
+})
+
+export default connect(undefined, mapDispatchToProps)(Header);
--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -6,3 +6,9 @@ test('should render login page', () => {
 	const wrapper = shallow(<LoginPage/>)
 	expect(wrapper).toMatchSnapshot();
 })
+test('should call startLogin on button click', () => {
+	const startLogin = jest.fn()
+	const wrapper = shallow(<LoginPage startLogin={startLogin}/>)
+	wrapper.find('button').simulate('click')
+	expect(startLogin).toHaveBeenCalled()
+})
